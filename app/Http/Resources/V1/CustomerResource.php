de880f82b307b88fff55a271cb08c2ae14631bb2
A1manNa1m <mohamadaimannaim@gmail.com>
2025-11-20 22:02:09 +0800

    filter func

--- app/Http/Resources/V1/CustomerResource.php
+++ app/Http/Resources/V1/CustomerResource.php
@@ -23,7 +23,7 @@ class CustomerResource extends JsonResource
             'city' => $this->city,
             'state' => $this->state,
             'postal_code' => $this->postal_code,
-
+            'invoices' => InvoiceResource::collection($this->whenLoaded('invoices')),
         ];
     }
 }
